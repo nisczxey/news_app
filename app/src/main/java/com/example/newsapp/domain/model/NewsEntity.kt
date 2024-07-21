bf9dfbae386d23da1791e29c3d6ae1f47c462fe5
carrotteddy <devcarrott@gmail.com>
2024-07-21 20:26:29 +0700

    structure project

---
+++ app/src/main/java/com/example/newsapp/domain/model/NewsEntity.kt
@@ -0,0 +1,5 @@
+package com.example.newsapp.domain.model
+
+data class NewsEntity(
+    val id: String
+)
---
+++ app/src/main/java/com/example/newsapp/domain/use_case/GetNewsListUseCase.kt
@@ -0,0 +1,4 @@
+package com.example.newsapp.domain.use_case
+
+class GetNewsListUseCase {
+}
